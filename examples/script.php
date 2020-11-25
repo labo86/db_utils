90ac35799eea4da0da284124e112290e2f610eb7
Edwin <edwrodrig@gmail.com>
2020-11-25 19:53:46 -0300

    Scripts

--- examples/script.php
+++ examples/script.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use labo86\db_utils\Util;
 use labo86\db_utils\PDO;
 
-include_once "phar://" . __DIR__ . "/db_utils/vendor/autoload.php";
+include_once "phar://" . __DIR__ . "/db_utils.phar/vendor/autoload.php";
 
 $db_name = "name";
 $host = "localhost";
@@ -12,6 +12,9 @@ $username = "user";
 $password = "password";
 $db = PDO::OpenMysql($db_name, $host, $username, $password);
 
-PDO::selectAll($db, 'SELECT * FROM TABLE WHERE value = :value', ['value' => 'hola']);
+$result = PDO::selectAll($db, 'SELECT * FROM TABLE WHERE value = :value', ['value' => 'hola']);
+foreach ( $result as $row ) {
+    print_r($row);
+}
 
 echo Util::UUID();
